ed72ac697e58bf139d5379f9db324ffc74b535d3
zigzags71 <43006123+zigzags71@users.noreply.github.com>
2026-01-05 14:11:44 +0200

    Fix Orientation import in viewer

--- ui-viewer/src/main/java/uiviewer/app/MainApp.java
+++ ui-viewer/src/main/java/uiviewer/app/MainApp.java
@@ -15,7 +15,7 @@ import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.Orientation;
+import javafx.geometry.Orientation;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
